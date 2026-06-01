f78d8983f79458ba71fdf9a456fe87021fcb9c16
Adi <b24ee1062@iitj.ac.in>
2026-06-02 02:23:07 +0530

    Add API Router Structure

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from backend.app.core.config import settings
+from backend.app.api.router import api_router
 
 app = FastAPI(
-    title="AI Engineering Workspace",
+    title=settings.APP_NAME,
+    debug=settings.DEBUG,
     version="0.1.0"
 )
 
+app.include_router(
+    api_router,
+    prefix=settings.API_V1_PREFIX
+)
+
+
 @app.get("/")
 async def root():
-    return {"status": "running"}
+    return {
+        "message": f"{settings.APP_NAME} is running"
+    }
